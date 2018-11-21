2713a555fd3f5e554563fd60ad36b1706b700708
Louie O'Grady <l.ogrady@gmail.com>
2018-11-21 19:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+
+function johnLennonFacts(array) {
+    array = [];
+    var factsOutput = [];
+    var i = 0;
+    while (i < array.length) { 
+      factsOutput.push(array[i] + "!!!" );
+      i++;
+}
+    return factsOutput;
+}
